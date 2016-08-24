8839785ab2bf8d6cbd9c11ad1848f303d0dfbdb3
scambo00 <wscambo@gmail.com>
2016-08-24 08:52:19 -0400

    Add files via upload

--- add-ons/wnrTriggers.js
+++ add-ons/wnrTriggers.js
@@ -1,9 +1,10 @@
+// Licensed under the MIT license, see LICENSE file.
+// Author: Will Scarangello (https://github.com/scambo00)
 module.exports = function(RED) {
-		
-	
     function wnrTriggersNode(config) {
         RED.nodes.createNode(this,config);
-		
+		this.config = config;
+        this.state = {};
         var node = this;
 		var globalContext = this.context().global;
 		var outmsg = {
